b428b18321de089e796f72ff576520bf8488bf2f
shuhei.takahashi <uhiaha888@gmail.com>
2019-01-19 16:29:12 +0900

    #10 つぶやき追加のユースケース

---
+++ tests/usecase/tweetadd/test_tweet_add_usecase.py
@@ -0,0 +1,72 @@
+# coding: utf-8
+from toitta.domain.tweet import TweetType
+from toitta.usecase.request import InvalidRequestObject
+from toitta.usecase.response import ResponseType
+from toitta.usecase.tweetadd.tweet_add_usecase import TweetAddRequest
+
+
+class TestTweedAddRequest:
+
+    def test_valid_request(self):
+        adict = {
+            'type': TweetType.NORMAL,
+            'user_id': 123,
+            'tweet': 'some tweet'
+        }
+
+        actual = TweetAddRequest.from_dict(adict)
+
+        assert isinstance(actual, TweetAddRequest)
+        assert bool(actual) is True
+        assert actual.type == TweetType.NORMAL
+        assert actual.user_id == 123
+        assert actual.tweet == 'some tweet'
+        assert actual.parent_id is None
+
+    def test_type_is_missing(self):
+        adict = {
+            'user_id': 123,
+            'tweet': 'some tweet'
+        }
+
+        actual = TweetAddRequest.from_dict(adict)
+
+        assert isinstance(actual, InvalidRequestObject)
+        assert bool(actual) is False
+        assert actual.has_errors() is True
+        assert actual.errors[0] == {'parameter': 'type', 'message': 'typeは必須です'}
+
+    def test_type_is_normal_and_tweet_is_missing(self):
+        adict = {
+            'type': TweetType.NORMAL,
+            'user_id': 123
+        }
+
+        actual = TweetAddRequest.from_dict(adict)
+
+        assert actual.errors[0] == {'parameter': 'tweet', 'message': 'つぶやきを指定してください'}
+
+    def test_too_long_tweet(self):
+        tweet = '0123456789012345678901234567890123456789' \
+              + '0123456789012345678901234567890123456789' \
+              + '0123456789012345678901234567890123456789' \
+              + '0123456789012345678901234567890123456789'
+        adict = {
+            'type': TweetType.NORMAL,
+            'user_id': 123,
+            'tweet': tweet
+        }
+
+        actual = TweetAddRequest.from_dict(adict)
+
+        assert actual.errors[0] == {'parameter': 'tweet', 'message': 'つぶやきは140文字以内'}
+
+    def test_user_id_is_missing(self):
+        adict = {
+            'type': TweetType.NORMAL,
+            'tweet': 'some tweet'
+        }
+
+        actual = TweetAddRequest.from_dict(adict)
+
+        assert actual.errors[0] == {'parameter': 'user_id', 'message': 'user_idは必須です'}
---
+++ toitta/usecase/tweetadd/tweet_add_usecase.py
@@ -0,0 +1,70 @@
+# coding: utf-8
+from toitta.domain.tweet import TweetType, TweetConstants, Tweet
+from toitta.usecase.request import ValidRequestObject, InvalidRequestObject
+from toitta.usecase.response import ResponseFailure, ResponseSuccess
+
+
+class TweetAddRequest(ValidRequestObject):
+
+    def __init__(self,
+                 tweet,
+                 type_,
+                 user_id,
+                 parent_id=None):
+        self.tweet = tweet
+        self.type = type_
+        self.user_id = user_id
+        self.parent_id = parent_id
+
+    @classmethod
+    def from_dict(cls, adict):
+        invalid_req = InvalidRequestObject()
+
+        if 'type' not in adict:
+            invalid_req.add_error('type', 'typeは必須です')
+            return invalid_req
+
+        if 'tweet' not in adict and adict['type'] == TweetType.NORMAL:
+            invalid_req.add_error('tweet', 'つぶやきを指定してください')
+
+        if len(adict.get('tweet', '')) > TweetConstants.MAX_LENGTH:
+            invalid_req.add_error('tweet', 'つぶやきは{}文字以内'.format(TweetConstants.MAX_LENGTH))
+
+        if 'user_id' not in adict:
+            invalid_req.add_error('user_id', 'user_idは必須です')
+
+        if invalid_req.has_errors():
+            return invalid_req
+
+        return cls(tweet=adict.get('tweet'),
+                   type_=adict.get('type'),
+                   user_id=adict.get('user_id'),
+                   parent_id=adict.get('parent_id'))
+
+
+class TweetAddUseCase(object):
+
+    def __init__(self, repository):
+        self.repository = repository
+
+    def execute(self, request):
+        if not request:
+            return ResponseFailure.build_from_invalid_request_object(request)
+
+        try:
+            # TODO ユーザーの存在チェック
+            # TODO parentの存在チェック
+            tweet = self._build_tweet(request)
+            tweet = self.repository.add_tweet(tweet)
+            response = ResponseSuccess(tweet)
+            return response
+        except Exception as e:
+            return ResponseFailure.build_system_error(e)
+
+    def _build_tweet(self, request):
+        return Tweet(id=None,
+                     tweet=request.tweet,
+                     type_=request.type,
+                     user_id=request.user_id,
+                     parent_id=request.parent_id)
+
